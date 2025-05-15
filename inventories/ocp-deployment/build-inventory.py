939cbd893eb3f843344089cb379df9d65e394bc7
Shai Oren <sobarzan@redhat.com>
2025-05-15 12:46:28 +0300

    Changed inventory to be dynamically built for ocp-deployment

---
+++ inventories/ocp-deployment/build-inventory.py
@@ -0,0 +1,57 @@
+import os
+import yaml
+import sys
+
+hostvars_dir = sys.argv[0] # path to the host_vars dir
+dest_file = "deploy-ocp-hybrid-multinode.yml"
+masters = []
+workers = []
+
+if os.path.exists(hostvars_dir) and os.path.isdir(hostvars_dir):
+    for filename in os.listdir(hostvars_dir):
+        if filename.startswith('master') and os.path.isfile(os.path.join(hostvars_dir, filename)):
+            masters.append(filename)
+        elif filename.startswith('worker') and os.path.isfile(os.path.join(hostvars_dir, filename)):
+            workers.append(filename)
+
+masters.sort()
+workers.sort()
+
+output_yaml = {
+    'nodes': {
+        'children': {
+            'masters': {},
+            'workers': {}
+        }
+    },
+    'masters': {
+        'hosts': {}
+    },
+    'workers': {
+        'hosts': {}
+    },
+    'bastions': {
+        'hosts': {
+            'bastion': {}
+        }
+    },
+    'hypervisors': {
+        'hosts': {
+            'hypervisor': {}
+        }
+    },
+    'vm_hosts': {
+        'children': {
+            'hypervisors': {}
+        }
+    }
+}
+
+output_yaml['masters']['hosts'] = {node: {} for node in masters}
+output_yaml['workers']['hosts'] = {node: {} for node in workers}
+
+
+with open(dest_file, 'w') as outfile:
+    yaml.dump(output_yaml, outfile, sort_keys=False)
+
+print(f"inventory {dest_file} file created successfully.")

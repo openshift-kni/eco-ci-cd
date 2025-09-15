52ee072cabc5f187dd5a2c00b96dc78a59a7cc58
Maxim Kovgan <makovgan@redhat.com>
2025-09-15 11:49:57 +0300

    ### job: sanity
    #### Shortly
    Iterate over inventory+playbook tuples and: run `ansible-playbook --check` on them
    detect:
    1. Typos/Syntax issues
    2. Role/Collection resolving
    
    #### TL;DR
    
    - wrapper in: `hack/sanity_tests.sh`, source `tests.bash`
    - `TUPLES` array of tuples in format: `${inventory}:${playbook}` in `tests.bash`
    
    TODO: Move to actual E2E/molecule testing
    
    Signed-off-by: Maxim Kovgan <makovgan@redhat.com>

---
+++ tests.bash
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# 1st file is inventory file, 2nd file is playbook
+export TUPLES=(
+    "inventories/ocp-deployment/build-inventory.py:playbooks/deploy-ocp-hybrid-multinode.yml"
+)
